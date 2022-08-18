1232a1e4f3860a80989a6fc257313ceaab2feda2
sahusoma <sahusoma@hotmail.com>
2022-08-17 20:06:05 -0500

    17-agosto

---
+++ src/main/java/sam/com/ejerc01/Matriz.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sam.com.ejerc01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author shsolano
+ */
+    static int fil;
+    static int col;
+public class Matriz {
+    
+}
+
+   public static void main(String[] args) {
+    
+    Operaciones op = new Operqacions();
+            System.out.println("Entre Nro filas: ");        
+            int fil =SC.nextDouble();
+            System.out.println("Entre Nro columnas : ");        
+            int col =SC.ne();
+            mat[1][1] = val;
+
+
+        //Clase_uno dt = new Clase_uno();
+        //dt.uno();
+         clase_3Matriz cm = new clase_3Matriz();
+         cm.matriz();
+        
+    }
+    public static class  clase_3Matriz{
+        public void matriz(){
+            double[][] mat = new double[2][2];
+            Scanner SC = new Scanner(System.in);
+            System.out.println("Elija la Valor: ");        
+            double val =SC.nextDouble();
+            mat[1][1] = val;
+            System.out.println("Matriz: "+ mat[1][1]);
+            
+            
+        }
+        public void matrizIdentidad(){
+            
+        }
+
+    }
